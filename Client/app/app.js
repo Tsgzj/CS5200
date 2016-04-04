fab0e1df7a78ea9a0e23ef27b6d1ffddf139da54
Tsgzj <wxsun1991@gmail.com>
2016-04-04 17:04:04 -0400

    Client updated

--- Client/app/app.js
+++ Client/app/app.js
@@ -15,21 +15,62 @@ config(function($stateProvider, $urlRouterProvider) {
       })
       .state('electronics', {
         url: "/category",
-        templateUrl: "category.html"
+        templateUrl: "static/category.html",
+        controller: function($scope, $http) {
+            $http({
+                method: "GET",
+                url: "http://127.0.0.1:5000/category?category=electronics"
+            }).then(function (response) {
+                $scope.items = response.data;
+            });
+        }
       })
       .state('fashion', {
-        url: "/category",
-        templateUrl: "category.html"
+          url: "/category",
+          templateUrl: "static/category.html",
+          controller: function ($scope, $http) {
+              $http({
+                  method: "GET",
+                  url: "http://127.0.0.1:5000/category?category=fashion"
+              }).then(function (response) {
+                  $scope.items = response.data;
+              });
+          }
       })
       .state('home', {
         url: "/category",
-        templateUrl: "category.html"
+        templateUrl: "static/category.html",
+        controller: function($scope, $http) {
+            $http({
+                method : "GET",
+                url : "http://127.0.0.1:5000/category?category=home"
+            }).then(function(response) {
+                $scope.items = response.data;
+            });
+        }
       })
       .state('inventory', {
         url: "/inventory",
-        templateUrl: "inventory.html",
+        templateUrl: "static/inventory.html",
         controller: function($scope){
-          $scope.items = ["A", "List", "Of", "Items"];
+          $scope.items = [{
+              "InventoryId": 2,
+              "Title": "Mouse",
+              "Description": "",
+              "Price": 68,
+              "Discount": 1,
+              "Category": "Electronics",
+              "Available": 15
+          },
+          {
+              "InventoryId": 1,
+              "Title": "Keyboard",
+              "Description": "",
+              "Price": 100,
+              "Discount": 0.95,
+              "Category": "Electronics",
+              "Available": 5
+          }];
         }
       })
 });
