6d79d25f99a0afdc317db0144000dccd8ae9d26f
Tsgzj <wxsun1991@gmail.com>
2016-04-07 15:48:11 -0400

    Updating Exiting Card

--- Client/app/app.js
+++ Client/app/app.js
@@ -112,19 +112,94 @@ myApp.controller('BannerCtrl', ['$scope', '$log', '$state', '$cookies', function
 }]);
 
 myApp.controller('ProfileCtrl', ['$scope', '$http', '$state', '$cookies', function($scope, $http, $state, $cookies) {
+    $scope.typeoptions = ["Visa", "Master", "AmericanExpress"];
+    $scope.cardType = $scope.typeoptions[0];
     $scope.addNewPayment = function() {
         var requestData = {};
-        requestData["UserId"]="test",
-        requestData["CardNumber"]="123456789877",
-        requestData["Address"]="361 Huntington Ave",
-        requestData["ExpirationDate"]="04/11/2016",
-        requestData["Type"]="Visa"
-        $http.post(server + "/user/payment", requestData).
-        success(
-            $state.go('profile')
-        ).
-        error(
-            window.alert("Failed to update payment.")
-        )
+        if($scope.preChk()) {
+            requestData["UserId"] = $cookies.get("uid"),
+                requestData["CardNumber"] = $scope.cardNumber,
+                requestData["Address"] = $scope.cardAddress,
+                requestData["ExpirationDate"] = $scope.expirationDate,
+                requestData["Type"] = $scope.cardType,
+                $http.post(server + "/user/payment", requestData).success(
+                    $state.go('profile')
+                ).error(
+                    window.alert("Failed to update payment.")
+                )
+        }
+    }
+    $scope.preChk = function() {
+        if($scope.cardNumber == null || $scope.cardAddress == null || $scope.expirationDate == null || $scope.cardType == null){
+            window.alert("Please fill all the columns");
+            return false;
+        }
+        if(!$scope.checkCard($scope.cardNumber)){
+            window.alert("Invalid Card Number");
+            return false;
+        }
+        if(!$scope.checkDateFormat($scope.expirationDate)){
+            window.alert("Invalid ExpirationDate");
+            return false;
+        }
+        return true;
+    }
+    $scope.checkCard = function(cardNumber){
+        if (cardNumber.length != 16) {
+            return false;
+        }
+        if(isNaN(cardNumber)) {
+            return false;
+        }
+        if (!$scope.checkCardNumber(cardNumber)) {
+            return false;
+        }
+        return true;
+    }
+    $scope.checkCardNumber = function(value){
+        // get from https://gist.github.com/DiegoSalazar/4075533
+        if (/[^0-9-\s]+/.test(value)) return false;
+
+        // The Luhn Algorithm. It's so pretty.
+        var nCheck = 0, nDigit = 0, bEven = false;
+        value = value.replace(/\D/g, "");
+
+        for (var n = value.length - 1; n >= 0; n--) {
+            var cDigit = value.charAt(n),
+                nDigit = parseInt(cDigit, 10);
+
+            if (bEven) {
+                if ((nDigit *= 2) > 9) nDigit -= 9;
+            }
+
+            nCheck += nDigit;
+            bEven = !bEven;
+        }
+
+        return (nCheck % 10) == 0;
+    }
+    $scope.checkDateFormat = function(dateString){
+        // get from http://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript
+        if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+            return false;
+
+        // Parse the date parts to integers
+        var parts = dateString.split("/");
+        var day = parseInt(parts[1], 10);
+        var month = parseInt(parts[0], 10);
+        var year = parseInt(parts[2], 10);
+
+        // Check the ranges of month and year
+        if(year < 1000 || year > 3000 || month == 0 || month > 12)
+            return false;
+
+        var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+        // Adjust for leap years
+        if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+            monthLength[1] = 29;
+
+        // Check the range of the day
+        return day > 0 && day <= monthLength[month - 1];
     }
 }])
